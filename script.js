48890b18f8f62feec6ee9b889fc47cd1021f7070
Mosejoro <monehinseun4125@gmail.com>
2024-07-22 02:36:31 +0100

    New

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // Load your JSON data (replace with your actual data loading logic)
 const pdfData = [
     { "name": "Meet Me", "summary": "Meeting Invitation" },
-    { "name": "Hello", "summary": "Greeting Message" },
+    { "name": "Hello", "summary": "What" },
+    { "name": "Kind", "summary": "Greeting Message" },
+    { "name": "Life", "summary": "Greeting Message" },
+    { "name": "Swim", "summary": "Greeting Message" },
+    { "name": "Hope", "summary": "Greeting Message" },
+    
     // ... (your other PDFs)
   ];
   
